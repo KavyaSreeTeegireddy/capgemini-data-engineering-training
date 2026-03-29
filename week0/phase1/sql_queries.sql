b5710eb72a32469e697697017b6b3bd50ef6be6d
KavyaSreeTeegireddy <161039645+KavyaSreeTeegireddy@users.noreply.github.com>
2026-03-29 10:54:29 +0530

    Add files via upload

---
+++ week0/phase1/sql_queries.sql
@@ -0,0 +1,16 @@
+CREATE TABLE customers (
+    customer_id INT,
+    customer_name VARCHAR(50),
+    city VARCHAR(50),
+    age INT
+);
+INSERT INTO customers VALUES
+(1, 'Ravi', 'Hyderabad', 25),
+(2, 'Sita', 'Chennai', 32),
+(3, 'Arun', 'Hyderabad', 28);
+
+1) SELECT * FROM customers;
+2) SELECT * FROM customers WHERE city='Chennai';
+3) SELECT * FROM customers WHERE age>25;
+4) SELECT customer_name,city FROM customers;
+5) SELECT city, COUNT(*) FROM customers GROUP BY city;
