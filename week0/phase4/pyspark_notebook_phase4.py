65f85688692c5196ff3aa746dd994469529609be
KavyaSreeTeegireddy <161039645+KavyaSreeTeegireddy@users.noreply.github.com>
2026-04-05 12:17:34 +0530

    Add files via upload

---
+++ week0/phase4/pyspark_notebook_phase4.py
@@ -0,0 +1,44 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, sum, count, when
+spark = SparkSession.builder.appName('Spark Playground').getOrCreate()
+
+customers = spark.read.option("header", "true").csv("/samples/customers.csv")
+sales=spark.read.option("header", "true").csv("/samples/sales.csv")
+
+sales_df = sales.withColumn("total_amount", col("total_amount").cast("int"))
+
+
+#Task 1: Daily Sales
+task1_df = sales_df.groupBy("sale_date").agg(sum("total_amount").alias("total_sales"))
+task1_df.show()
+
+
+#Task 2:City-wise Revenue
+task2_df = customers.join(sales_df, "customer_id").groupBy("city").agg(sum("total_amount").alias("total_revenue_gen"))
+task2_df.show()
+
+
+#Task 3: Top 5 Customers
+task3_df = customers.join(sales_df, "customer_id").groupBy("first_name").agg(sum("total_amount").alias("total_amount_spend")).orderBy(col("total_amount_spend").desc()).limit(5)
+task3_df.show()
+
+
+#Task 4: Repeated Customers (>1 order)
+task4_df = sales_df.groupBy("customer_id").agg(count("*").alias("countof_orders")).filter(col("countof_orders") > 1)
+task4_df.show()
+
+
+#Task 5: Customer Segmentation → Create business logic: total_spend > 10000 → Gold 5000–10000 → Silver < 5000 → Bronze
+task5_df = customers.join(sales_df, "customer_id").groupBy("first_name").agg(sum("total_amount").alias("total_amount_spend")).withColumn("segment",when(col("total_amount_spend") > 100, "Gold").when(col("total_amount_spend") >= 50, "Silver").otherwise("Bronze")
+    )
+task5_df.show()
+
+
+#Task 6: Final Reporting Table 
+final_df = customers.join(sales_df, "customer_id").groupBy("first_name", "city") \
+    .agg(sum("total_amount").alias("total_amount_spend"),count("sale_id").alias("countof_orders")) .withColumn("segment",when(col("total_amount_spend") > 100, "Gold").when(col("total_amount_spend") >= 50, "Silver").otherwise("Bronze"))
+final_df.show()
+
+
+#Task 7: Save Output 
+final_df.write.mode("overwrite").csv("/samples/output/report")
